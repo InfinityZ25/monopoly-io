e9d27fb8c6eab2beac5e8e48c5856d056bb35082
Charles Campbell <charles@cc5tudio.com>
2017-11-30 11:04:05 -0500

    added a start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
@@ -9,6 +10,10 @@ import { SquareComponent } from './square/square.component';
 import { DeckComponent } from './deck/deck.component';
 import { SpaceComponent } from './space/space.component';
 import { CornerSpaceComponent } from './corner-space/corner-space.component';
+import { StartComponent } from './start/start.component';
+import { GameBoardComponent } from './game-board/game-board.component';
+import { SettingsComponent } from './settings/settings.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 
 @NgModule({
@@ -18,11 +23,16 @@ import { CornerSpaceComponent } from './corner-space/corner-space.component';
     SquareComponent,
     DeckComponent,
     SpaceComponent,
-    CornerSpaceComponent
+    CornerSpaceComponent,
+    StartComponent,
+    GameBoardComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
